1eaa57e11dcf589877096be25f83bbd634c70f47
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-04-17 20:37:35 +0200

    fix: some minor fixes refactoring code to get it to work

--- models/state.py
+++ models/state.py
@@ -10,25 +10,27 @@ import os
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="state")
 
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        # DBStorage
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete", backref="state")
         cities = relationship("City", cascade="all, delete", backref="state")
     else:
-        # FileStorage
-        @property
-        def cities(self):
-            """Getter attribute cities"""
-            city_list = []
-            for city in storage.all(City).values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
-        
+        name = ""
+
     def __init__(self, *args, **kwargs):
         """Initializes the state"""
         super().__init__(*args, **kwargs)
 
+
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Getter attribute cities"""
+            c_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    c_list.append(city)
+            return c_list
+        
