015134efd9fd05d56d7dfd1edb6e3ed9d2baff1d
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-05-03 23:39:13 +0300

    task 0-setup done

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Install nginx and create required folders
+
+sudo apt-get -y update
+sudo apt-get -y install nginx
+
+sudo mkdir -p /data/
+sudo mkdit -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "Hello world" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i "38i \ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+
+sudo service nginx restart
+exit 0
