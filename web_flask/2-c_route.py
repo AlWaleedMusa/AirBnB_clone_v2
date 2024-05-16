2a0cca24c1b415d1121a5ca3ca9bd5a66b4e492b
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-05-16 11:06:48 +0300

    task 2 done

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """This function returns a string when the route / is hit"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """This function returns a string when the route /hbnb is hit"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    """This function returns a string when the route /c/<text> is hit
+    and replacing _ with spaces"""
+    return "C " + text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run()
