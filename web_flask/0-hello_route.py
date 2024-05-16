c8bd5875924f3ce9cd73b9afd14d1f21582acd97
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-05-16 11:00:58 +0300

    task 0 done

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """This function returns a string when the route / is hit"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run()
