e5c9ac38bf26d7b3172e9d8c3674d9b99642c08f
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-05-16 11:09:48 +0300

    task 3 done

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """This function returns a string when the route / is hit"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """This function returns a string when the route /hbnb is hit"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    """This function returns a string when the route /c/<text> is hit
+    and replacing _ with spaces"""
+    return "C " + text.replace("_", " ")
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python", strict_slashes=False)
+def python_is_cool(text="is cool"):
+    """This function returns a string when the route /python/<text> is hit
+    and replacing _ with spaces"""
+    return "Python " + text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run()
